e3f7984d37c5e2f28cd9f71589a5f0927516f26e
cu-e <cu.e@arelon.org>
2025-06-10 23:55:26 +0500

    add empty main page file

--- frontend/src/routes/router.tsx
+++ frontend/src/routes/router.tsx
@@ -1,11 +1,12 @@
 import {
   createBrowserRouter,
 } from "react-router";
+import MainPage from "../components/pages/Main/MainPage";
 
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>MainPage</div>,
+    element: <MainPage/>,
   },
 ]);
